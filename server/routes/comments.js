b3cd5f0a1071afcf87d5c3d2e52a2b81a194ea5f
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-02-24 14:35:39 +0300

    Add, delete and get comments

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,7 +1,12 @@
 import express from "express";
-import {  } from "../controllers/comment.js";
+import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import { verifyToken } from "../verifyToken.js";
+
 
 const router = express.Router();
 
+router.post("/",verifyToken ,addComment)
+router.delete("/:id",verifyToken ,deleteComment)
+router.get("/:videoId",verifyToken ,getComments)
 
 export default router;
