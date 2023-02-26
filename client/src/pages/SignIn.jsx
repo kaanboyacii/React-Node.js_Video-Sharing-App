1751b51060552196ce5154bf50337fcfa4bee302
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-02-26 18:33:23 +0300

    React-nodejs mongodb auth (login / register)

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import styled from "styled-components";
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -64,18 +64,31 @@ const Link = styled.span`
 `;
 
 const SignIn = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async (e) =>{
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/signin", {name,password});
+      console.log(res)
+    } catch (err) {
+      
+    }
+  }
+
   return (
     <Container>
       <Wrapper>
         <Title>Sign in</Title>
         <SubTitle>to continue to LamaTube</SubTitle>
-        <Input placeholder="username" />
-        <Input type="password" placeholder="password" />
-        <Button>Sign in</Button>
+        <Input placeholder="username" onChange={e=>setName(e.target.value)} />
+        <Input type="password" placeholder="password"  onChange={e=>setPassword(e.target.value)} />
+        <Button onClick={handleLogin}>Sign in</Button>
         <Title>or</Title>
-        <Input placeholder="username" />
-        <Input placeholder="email" />
-        <Input type="password" placeholder="password" />
+        <Input placeholder="username"  onChange={e=>setName(e.target.value)}/>
+        <Input placeholder="email"  onChange={e=>setEmail(e.target.value)}/>
+        <Input type="password" placeholder="password"  onChange={e=>setPassword(e.target.value)} />
         <Button>Sign up</Button>
       </Wrapper>
       <More>
